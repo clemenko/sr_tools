00d4f11980fc07a58cccc67cfe3e89924144e699
Andy Clemenko <clemenko@gmail.com>
2020-11-20 10:19:15 -0500

    update for adding kernerl support tar for offline

--- stackrox_offline/getoffline_stackrox.sh
+++ stackrox_offline/getoffline_stackrox.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#vars
+# vars
 export version=3.0.52.0
 export username=andy@stackrox.com
 
@@ -9,20 +9,24 @@ rm -rf *.tar.gz
 echo -n "PASSWORD: "; stty -echo; read password; stty echo; echo
 # passwd= # get rid of passwd
 
-#get images - includes roxctl
+# get images - includes roxctl
 echo -n " getting image bundle for $version : "
 curl -#L -u $username:$password https://install.stackrox.io/$version/image-bundle.tgz -o stackrox_offline_$version.tgz
 
-#get collector packages
+# get collector packages
 echo -n " getting image collector bundle for $version : "
 curl -#L -u $username:$password https://install.stackrox.io/$version/image-collector-bundle.tgz -o image-collector-bundle_$version.tgz
 
 echo -n " getting default auth plugin for $version : "
 curl -#L -u $username:$password https://install.stackrox.io/authz-plugin/default-authz-plugin-1.0-src.zip -o default-authz-plugin-1.0-src.zip
 
-#get scanner db
+# get scanner db
 echo -n " getting vuln database : "
-curl -#LO -u $username:$password -L https://install.stackrox.io/scanner/scanner-vuln-updates.zip
+curl -#LO -u $username:$password https://install.stackrox.io/scanner/scanner-vuln-updates.zip
+
+# get kernel modules
+echo -n "getting kernel support packages"
+curl -#LO -u $username:$password $(curl -sL -u $username:$password https://install.stackrox.io/collector/support-packages/index.html |grep .zip | head -1 | awk -F'"' '{print $4}')
 
 #compressing
 cd ..; tar --exclude=.DS_Store --exclude=all_the_things_* -zcvf stackrox_offline/all_the_things_$version.tar.gz stackrox_offline; cd stackrox_offline
